fc41d0a3d03088fdf982f0e1c8d75458b100e0a5
migue <miguelhernandez.gonzalez28@gmail.com>
2023-03-08 22:30:28 -0300

    ejesm

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import LectorQR from './pages/lectorQR';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LectorQR/>
   </React.StrictMode>
 );
 
